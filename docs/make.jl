0a1936e09dff0b27d503e80c7bdc22a6deaffbba
Ivan Utkin <iutkin@ethz.ch>
2026-06-09 19:51:54 +0200

    Add formatter CI workflow

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,7 @@ using DemTools
 using Documenter
 using DocumenterVitepress
 
-DocMeta.setdocmeta!(DemTools, :DocTestSetup, :(using DemTools); recursive = true) # Keep doctest snippets short.
+DocMeta.setdocmeta!(DemTools, :DocTestSetup, :(using DemTools); recursive=true) # Keep doctest snippets short.
 
 repo = "github.com/utkinis/DemTools.jl"
 devbranch = "main"
@@ -10,28 +10,23 @@ devurl = "dev"
 deploy_url = "https://utkinis.github.io/DemTools.jl/"
 
 makedocs(;
-    sitename = "DemTools.jl",
-    authors = "Ivan Utkin",
-    modules = [DemTools],
-    format = DocumenterVitepress.MarkdownVitepress(;
-        repo,
-        devbranch,
-        devurl,
-        deploy_url,
-        description = "Tools for processing digital elevation models in Julia.",
-    ),
-    pages = [
-        "Getting started" => "index.md",
-        "DEM filtering" => "filtering.md",
-        "API reference" => "api.md",
-    ],
-    checkdocs = :exports,
-)
+         sitename="DemTools.jl",
+         authors="Ivan Utkin",
+         modules=[DemTools],
+         format=DocumenterVitepress.MarkdownVitepress(;
+                                                      repo,
+                                                      devbranch,
+                                                      devurl,
+                                                      deploy_url,
+                                                      description="Tools for processing digital elevation models in Julia."),
+         pages=["Getting started" => "index.md",
+                "DEM filtering" => "filtering.md",
+                "API reference" => "api.md"],
+         checkdocs=:exports)
 
 DocumenterVitepress.deploydocs(;
-    repo,
-    target = joinpath(@__DIR__, "build"),
-    branch = "gh-pages",
-    devbranch,
-    push_preview = true,
-)
+                               repo,
+                               target = joinpath(@__DIR__, "build"),
+                               branch = "gh-pages",
+                               devbranch,
+                               push_preview=true)
